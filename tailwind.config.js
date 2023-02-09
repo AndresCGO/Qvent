5251af790ce4230993d0f95f082457bc8f45b418
sam1rC <samir.colpas@hotmail.com>
2023-02-09 09:48:51 -0500

    add logo and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'cream':'#e5d9b6',
+        'green-grass':'#5f8d4e',
+        'brown':'#3c2a21',
+        'green-pine':'#285430',
+      }
+    },
   },
   plugins: [],
 }
