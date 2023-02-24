8c077fb418a7cae9c631bafab004bf08f4c293cc
sam1rC <samir.colpas@hotmail.com>
2023-02-24 16:22:22 -0500

    add displayEvents function

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,11 @@
+const main_container = document.getElementById('main_container');
+let items;
+window.onload = ()=>{
+    items = getAllEvents();
+    displayEvents(main_container,items);
+}
+
+
 function getAllEvents()
 {
     let values = [],
@@ -15,4 +23,15 @@ function getAllEvents()
     return values;
 }
 
-console.log(getAllEvents());
+function displayEvents(container,events)
+{
+    for(let i=0;i<events.length;i++)
+    {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        const card_title = document.createElement('p');
+        card_title.textContent = events[i].name;
+        card.appendChild(card_title);
+        container.appendChild(card);
+    }
+}
