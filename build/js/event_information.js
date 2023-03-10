e7e95ffba0d99ae28ea4ac7ca7ed2911f8a853e8
sam1rC <samir.colpas@hotmail.com>
2023-03-10 14:43:35 -0500

    change fps

--- build/js/event_information.js
+++ build/js/event_information.js
@@ -3,6 +3,7 @@ const read_tickets_number = document.getElementById('read_tickets_number');
 const event_name = document.getElementById('event_name');
 const generate_tickets = document.getElementById('generate_tickets');
 const generated_tickets = document.getElementById('generated_tickets');
+const read_ticket = document.getElementById('read_ticket');
 let current_event;
 
 function getCurrentEvent()
@@ -25,6 +26,9 @@ window.onload = ()=>{
     });
     generated_tickets.addEventListener('click',()=>{
         window.location.href = "../build/generated_tickets.html";
+    });
+    read_ticket.addEventListener('click',()=>{
+        window.location.href = "../build/read_ticket.html";
     });
 }
 
--- build/js/read_ticket.js
+++ build/js/read_ticket.js
@@ -1,10 +1,10 @@
 const scanner = new Html5QrcodeScanner('reader', { 
     // Scanner will be initialized in DOM inside element with id of 'reader'
     qrbox: {
-        width: 250,
-        height: 250,
+        width: 150,
+        height: 150,
     },  // Sets dimensions of scanning box (set relative to reader element width)
-    fps: 20, // Frames per second to attempt a scan
+    fps: 10, // Frames per second to attempt a scan
 });
 
 console.log(scanner);
