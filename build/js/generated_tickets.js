ff8ec9edbc8643a1f6cb2bd828efb0c59012083e
andres <andresc.gomez@upb.edu.co>
2023-03-12 21:28:06 -0500

    add styles to qr codes

--- build/js/generated_tickets.js
+++ build/js/generated_tickets.js
@@ -23,9 +23,28 @@ function getQrCodes(tickets)
 {
     for(let i=0;i<tickets.length;i++)
     {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        const date = document.createElement('label');
+        date.textContent = "Fecha y hora";
+        date.classList.add('date');
+        card.appendChild(date)
+        const options = document.createElement('img');
+        options.src = 'img/options.png';
+        options.setAttribute('style','-o-object-fit: scale-down; object-fit: scale-down; position: absolute; right: 1.25rem;');
+        card.appendChild(options)
         const qr_image = document.createElement('img');
         qr_image.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + tickets[i];
-        qr_image.setAttribute('style', 'width: 10rem; height: auto;');
-        main_container.appendChild(qr_image);
+        qr_image.setAttribute('style', 'width: 10rem; height: auto; border-radius: 0.5rem;');
+        card.appendChild(qr_image)
+        const code = document.createElement('label');
+        code.textContent = "xxxx-xxxx-xxxx";
+        code.classList.add('code');
+        card.appendChild(code)
+        const share = document.createElement('img');
+        share.src = 'img/compartir.png';
+        share.classList.add('imgshare');
+        card.appendChild(share)
+        main_container.appendChild(card);
     }
 }
