10e1791ebf48b2c9d5dcf7e1699049024e79493f
sam1rC <samir.colpas@hotmail.com>
2023-03-14 11:31:37 -0500

    add async functions

--- build/js/generated_tickets.js
+++ build/js/generated_tickets.js
@@ -19,43 +19,49 @@ window.onload = ()=>{
     getQrCodes(tickets);
 }
 
-function getQrCodes(tickets)
+async function getQrCodes(tickets)
 {
     for(let i=0;i<tickets.length;i++)
     {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        const date = document.createElement('label');
-        date.textContent = new Date().toLocaleDateString();
-        date.classList.add('date');
-        card.appendChild(date)
-        const options = document.createElement('img');
-        options.src = 'img/options.png';
-        options.setAttribute('style','-o-object-fit: scale-down; object-fit: scale-down; position: absolute; right: 1.25rem;');
-        card.appendChild(options)
-        const qr_image = document.createElement('img');
-        qr_image.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + tickets[i];
-        qr_image.setAttribute('style', 'width: 10rem; height: auto; border-radius: 0.5rem;');
-        card.appendChild(qr_image)
-        const code = document.createElement('label');
-        code.textContent = `${i+1}`;
-        code.classList.add('code');
-        card.appendChild(code)
-        const share = document.createElement('img');
-        share.src = 'img/compartir.png';
-        share.setAttribute('style', 'width: 2rem; height: auto; display: block; margin-right: auto; margin-left: auto;');
-        share.classList.add('imgshare');
-        share.addEventListener('click',()=>
-        {
-            if(navigator.share)
-            {
-                navigator.share(qr_image).then(()=>
-                {
-                    alert('QR Compartido!');
-                }).catch(console.error);
-            }
-        });
-        card.appendChild(share)
-        main_container.appendChild(card);
+        await createQRCard(tickets[i],i);
     }
+}
+
+
+async function createQRCard(value,i)
+{
+    const card = document.createElement('div');
+    card.classList.add('card');
+    const date = document.createElement('label');
+    date.textContent = new Date().toLocaleDateString();
+    date.classList.add('date');
+    card.appendChild(date)
+    const options = document.createElement('img');
+    options.src = 'img/options.png';
+    options.setAttribute('style','-o-object-fit: scale-down; object-fit: scale-down; position: absolute; right: 1.25rem;');
+    card.appendChild(options)
+    const qr_image = document.createElement('img');
+    qr_image.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + value;
+    qr_image.setAttribute('style', 'width: 10rem; height: auto; border-radius: 0.5rem;');
+    card.appendChild(qr_image)
+    const code = document.createElement('label');
+    code.textContent = `${i+1}`;
+    code.classList.add('code');
+    card.appendChild(code)
+    const share = document.createElement('img');
+    share.src = 'img/compartir.png';
+    share.setAttribute('style', 'width: 2rem; height: auto; display: block; margin-right: auto; margin-left: auto;');
+    share.classList.add('imgshare');
+    share.addEventListener('click',()=>
+    {
+        if(navigator.share)
+        {
+            navigator.share(qr_image).then(()=>
+            {
+                alert('QR Compartido!');
+            }).catch(console.error);
+        }
+    });
+    card.appendChild(share);
+    main_container.appendChild(card);
 }
